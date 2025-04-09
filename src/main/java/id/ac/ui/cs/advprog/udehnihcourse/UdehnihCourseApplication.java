e76221662ab06ff741b99809459feaa668aa5360
Dardrich <dardrich1465@gmail.com>
2025-04-09 12:01:16 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/UdehnihCourseApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdehnihCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdehnihCourseApplication.class, args);
+    }
+
+}
