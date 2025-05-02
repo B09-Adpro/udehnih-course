3295781e472c6c4ee69e330a9f42ae089244098a
Muhammad Naufal Ramadhan <naufalnauf04@gmail.com>
2025-05-02 20:24:05 +0700

    [GREEN] Implement EnrollmentRepository.

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/repository/EnrollmentRepository.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.udehnihcourse.repository;
+
+import id.ac.ui.cs.advprog.udehnihcourse.model.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    List<Enrollment> findByStudentId(String studentId);
+    Optional<Enrollment> findByStudentIdAndCourseId(String studentId, Long courseId);
+    boolean existsByStudentIdAndCourseId(String studentId, Long courseId);
+}
