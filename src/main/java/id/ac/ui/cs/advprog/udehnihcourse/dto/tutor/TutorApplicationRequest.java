c31c56f09aa8c77e52c31ed018f7bcf72b4bc236
Dardrich <dardrich1465@gmail.com>
2025-05-09 16:24:35 +0700

    feat: add interval validation to TutorApplicationRequest

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/dto/tutor/TutorApplicationRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/dto/tutor/TutorApplicationRequest.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.udehnihcourse.dto.tutor;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -8,7 +10,15 @@ import lombok.Data;
  */
 @Data
 public class TutorApplicationRequest {
+    @NotBlank(message = "Experience is mandatory")
+    @Size(max = 2000, message = "Experience cannot exceed 2000 characters")
     private String experience;
+
+    @NotBlank(message = "Qualifications are mandatory")
+    @Size(max = 1000, message = "Qualifications cannot exceed 1000 characters")
     private String qualifications;
+
+    @NotBlank(message = "Bio is mandatory")
+    @Size(max = 500, message = "Bio cannot exceed 500 characters")
     private String bio;
 }
