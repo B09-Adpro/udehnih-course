3f5546a2632011f91663cfc80f6da337bc65b95f
Dardrich <dardrich1465@gmail.com>
2025-05-09 16:57:00 +0700

    feat: add ArticleRepository

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/repository/ArticleRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/repository/ArticleRepository.java
@@ -3,8 +3,11 @@ package id.ac.ui.cs.advprog.udehnihcourse.repository;
 import id.ac.ui.cs.advprog.udehnihcourse.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long>{
-
+    List<Article> findBySectionId(Long sectionId);
+    Optional<Article> findByIdAndSectionId(Long articleId, Long sectionId);
 }
