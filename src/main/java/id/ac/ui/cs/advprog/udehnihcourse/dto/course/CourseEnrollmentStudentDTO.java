91527c90afcc23a5f06a6ae9a5e49af216845857
Dardrich <dardrich1465@gmail.com>
2025-05-09 16:30:46 +0700

    feat: add CourseEnrollmentStudentDTO

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/dto/course/CourseEnrollmentStudentDTO.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.udehnihcourse.dto.course;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseEnrollmentStudentDTO {
+    private String studentId;
+    private String studentName; // TODO: Tarik dari Auth
+    private LocalDateTime enrolledAt; // TODO: Tarik dari Enrollment
+}
