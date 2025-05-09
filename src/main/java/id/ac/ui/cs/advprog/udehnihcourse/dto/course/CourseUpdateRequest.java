e9e0a85c04cbf7bff7739284e7cbdcd97ffb862a
Dardrich <dardrich1465@gmail.com>
2025-05-09 16:23:07 +0700

    feat: add internal validation to CourseUpdateRequest

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/dto/course/CourseUpdateRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/dto/course/CourseUpdateRequest.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.udehnihcourse.dto.course;
 
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.math.BigDecimal;
 
@@ -9,7 +11,12 @@ import java.math.BigDecimal;
  */
 @Data
 public class CourseUpdateRequest {
+    @Size(max = 255, message = "Title cannot exceed 255 characters")
     private String title;
+
+    @Size(max = 1000, message = "Description cannot exceed 1000 characters")
     private String description;
+
+    @PositiveOrZero(message = "Price must be zero or positive")
     private BigDecimal price;
 }
