b9ae7d61d6c9b7d752254184ccbce259b46a2e83
Dardrich <dardrich1465@gmail.com>
2025-05-09 16:57:15 +0700

    feat: add SectionRepository

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/repository/SectionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/repository/SectionRepository.java
@@ -3,8 +3,11 @@ package id.ac.ui.cs.advprog.udehnihcourse.repository;
 import id.ac.ui.cs.advprog.udehnihcourse.model.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SectionRepository extends JpaRepository<Section, Long> {
-
+    List<Section> findByCourseId(Long courseId);
+    Optional<Section> findByIdAndCourseId(Long sectionId, Long courseId);
 }
