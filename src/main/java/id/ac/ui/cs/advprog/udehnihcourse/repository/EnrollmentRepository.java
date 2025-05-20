95532582410b5ade009a7718ce6dcc89a9177190
Dardrich <dardrich1465@gmail.com>
2025-05-21 00:33:04 +0700

    copy EnrollmentRepository and add several methods

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/repository/EnrollmentRepository.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.udehnihcourse.repository;
+
+import id.ac.ui.cs.advprog.udehnihcourse.model.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    List<Enrollment> findByStudentId(Long studentId);
+    Optional<Enrollment> findByStudentIdAndCourseId(Long studentId, Long courseId);
+    boolean existsByStudentIdAndCourseId(Long studentId, Long courseId);
+    long countByCourseId(Long courseId);
+    List<Enrollment> findByCourseId(Long courseId);
+}
