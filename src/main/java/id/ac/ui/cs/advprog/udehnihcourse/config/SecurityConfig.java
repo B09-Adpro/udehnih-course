68c08b743c535d08c56bd24c26de99e5f4a70d71
Dardrich <dardrich1465@gmail.com>
2025-05-20 11:28:34 +0700

    feat: add SecurityConfig

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.udehnihcourse.config;
+
+import id.ac.ui.cs.advprog.udehnihcourse.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                 .requestMatchers(HttpMethod.POST,
+                                         "/api/tutors/apply")
+                                     .hasRole("STUDENT") // Hanya Student yang bisa apply
+                                .requestMatchers(HttpMethod.GET,
+                                        "/api/tutors/status")
+                                .hasAnyRole("STUDENT", "TUTOR") // Student dan Tutor yang bisa cek status dia diterima atau ngga
+                                .requestMatchers(HttpMethod.DELETE,
+                                        "/api/tutors/apply")
+                                .hasRole("STUDENT") // Hanya Student yang bisa menggagalkan applicancenya sendiri
+                                .requestMatchers(HttpMethod.GET,
+                                        "/api/tutors/course")
+                                .hasRole("TUTOR") // Hanya Tutor yang bisa cek courses-nya sendiri
+
+
+                                .anyRequest().authenticated()
+                );
+//                .authorizeHttpRequests(authorizeRequests ->
+//                                authorizeRequests.anyRequest().permitAll()
+//                );
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
