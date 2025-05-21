a74d8a70e1734970cbcadf6bbd78de3161dd6fc4
Dardrich <dardrich1465@gmail.com>
2025-05-22 00:25:39 +0700

    feat: add feign in main app

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/UdehnihCourseApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/UdehnihCourseApplication.java
@@ -2,8 +2,10 @@ package id.ac.ui.cs.advprog.udehnihcourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UdehnihCourseApplication {
 
     public static void main(String[] args) {
