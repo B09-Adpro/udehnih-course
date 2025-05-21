9cfef7b7fc84bebc08807798675e412700ead693
Muhammad Naufal Ramadhan <naufalnauf04@gmail.com>
2025-05-21 20:41:51 +0700

    [GREEN] Implement PaymentRequestDTO.

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/dto/coursebrowsing/PaymentRequestDTO.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.udehnihcourse.dto.coursebrowsing;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequestDTO {
+    private Long studentId;
+    private Long courseId;
+    private BigDecimal amount;
+    private Long timestamp;
+}
