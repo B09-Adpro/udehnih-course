cb94ee513529768b906de02d39b9d7c9c6cde149
Dardrich <dardrich1465@gmail.com>
2025-05-22 17:59:55 +0700

    feat: edit EnrollmentStatus

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/model/EnrollmentStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/model/EnrollmentStatus.java
@@ -1,7 +1,8 @@
 package id.ac.ui.cs.advprog.udehnihcourse.model;
 
 public enum EnrollmentStatus {
-    ENROLLED,
-    COMPLETED,
-    DROPPED
+    ENROLLED, // Pembayaran sudah berhasil diverifikasi Staff
+    PENDING, // Default value, masih onprogress payment
+    DROPPED, // Student unenroll course yang sempat di-enroll-nya
+    PAYMENT_FAILED // Pembayaran gagal
 }
