266dbdf98c8c4017075263c51bd87d52a2624d66
Muhammad Naufal Ramadhan <naufalnauf04@gmail.com>
2025-05-23 19:36:20 +0700

    [REFACTOR] feat: implement decorator to assertNotNull.

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/dto/coursebrowsing/PaymentRequestDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/dto/coursebrowsing/PaymentRequestDTO.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.udehnihcourse.dto.coursebrowsing;
 
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +15,22 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentRequestDTO {
+    @NotNull(message = "Enrollment ID is required")
     private Long enrollmentId;
+
+    @NotNull(message = "Student ID is required")
     private Long studentId;
+
+    @NotNull(message = "Course ID is required")
     private Long courseId;
+
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be positive")
     private BigDecimal amount;
+
+    @NotBlank(message = "Payment method is required")
     private String paymentMethod;
+
+    @NotNull(message = "Timestamp is required")
     private Long timestamp;
 }
