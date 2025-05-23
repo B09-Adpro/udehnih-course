fe04d6a98d005477301c02e7fdd97645bbe56f7b
Dardrich <dardrich1465@gmail.com>
2025-05-23 19:58:55 +0700

    feat: add prep for future async

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/config/FeignConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.udehnihcourse.config;
 
+import feign.Request;
 import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class FeignConfig {
     @Bean
     public RequestInterceptor requestInterceptor() {
         return new FeignRequestInterceptor();
+    }
+
+    @Bean
+    public Request.Options requestOptions() {
+        return new Request.Options(5000, 10000, true); // 5s connect, 10s read
     }
 }
