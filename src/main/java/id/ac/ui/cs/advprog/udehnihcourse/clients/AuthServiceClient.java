da6ec5a1517ab588ede6647a9e9b842cb5ea6d70
Dardrich <dardrich1465@gmail.com>
2025-05-23 19:59:18 +0700

    feat: add addRoleToUser auth

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/clients/AuthServiceClient.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/clients/AuthServiceClient.java
@@ -1,10 +1,16 @@
 package id.ac.ui.cs.advprog.udehnihcourse.clients;
 
 import id.ac.ui.cs.advprog.udehnihcourse.config.FeignConfig;
+import id.ac.ui.cs.advprog.udehnihcourse.dto.auth.RoleRequest;
+import id.ac.ui.cs.advprog.udehnihcourse.dto.auth.RoleResponse;
 import id.ac.ui.cs.advprog.udehnihcourse.dto.auth.UserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.concurrent.CompletableFuture;
 
 @FeignClient(
         name = "auth-service",
@@ -15,4 +21,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface AuthServiceClient {
     @GetMapping("/api/users/{userId}")
     UserInfoResponse getUserInfoById(@PathVariable("userId") String userId);
+
+    @GetMapping("/api/users/{userId}")
+    CompletableFuture<UserInfoResponse> getUserInfoByIdAsync(@PathVariable("userId") String userId);
+
+    @PostMapping("/api/roles/add")
+    RoleResponse addRoleToUser(@RequestBody RoleRequest roleRequest);
 }
