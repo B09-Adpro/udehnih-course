90e10ec9fdcf529e6d2458eb4fd03cbe63dcbcda
Muhammad Naufal Ramadhan <naufalnauf04@gmail.com>
2025-05-25 10:34:59 +0700

    feat: Implement AuthenticationPrincipal to get authenticated user details and fix path.

--- src/main/java/id/ac/ui/cs/advprog/udehnihcourse/controller/EnrollmentManagementController.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihcourse/controller/EnrollmentManagementController.java
@@ -1,12 +1,13 @@
 package id.ac.ui.cs.advprog.udehnihcourse.controller;
 
 import id.ac.ui.cs.advprog.udehnihcourse.dto.coursebrowsing.*;
-import id.ac.ui.cs.advprog.udehnihcourse.exception.EnrollmentNotFoundException;
+import id.ac.ui.cs.advprog.udehnihcourse.security.AppUserDetails;
 import id.ac.ui.cs.advprog.udehnihcourse.service.CourseEnrollmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -23,22 +24,20 @@ public class EnrollmentManagementController {
 
     private final CourseEnrollmentService enrollmentService;
 
-    // TODO : Exception Handling for Already Enrolled, Unauthorized JWT, Courses Not Found (CHECK API DOCS)
     @PostMapping
     public ResponseEntity<EnrollmentDTO> enrollInCourse(
-            @RequestHeader("Authorization") String token,
             @RequestParam Long courseId,
-            @RequestParam(name = "payment_method") String paymentMethod) {
-        Long studentId = extractStudentIdFromToken(token);
+            @RequestParam(name = "payment_method") String paymentMethod,
+            @AuthenticationPrincipal AppUserDetails userDetails){
+        Long studentId = userDetails.getId();
         EnrollmentDTO enrollmentDTO = enrollmentService.enrollStudentInCourse(studentId, courseId, paymentMethod);
         return ResponseEntity.ok(enrollmentDTO);
     }
 
-    // TODO : Exception Handling for Unauthorized JWT (CHECK API DOCS)
     @GetMapping
     public ResponseEntity<Map<String,  List<EnrolledCourseDTO>>> getEnrolledCourses(
-            @RequestHeader("Authorization") String token) {
-        Long studentId = extractStudentIdFromToken(token);
+            @AuthenticationPrincipal AppUserDetails userDetails) {
+        Long studentId = userDetails.getId();
         List<EnrolledCourseDTO> enrolledCourse = enrollmentService.getStudentEnrollments(studentId);
 
         Map<String,  List<EnrolledCourseDTO>> response = new HashMap<>();
@@ -47,18 +46,9 @@ public class EnrollmentManagementController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/payment-callback")
+    @PostMapping("payment-callback")
     public ResponseEntity<Map<String, Object>> handlePaymentCallback(
-            @RequestHeader(value = "X-API-Key", required = true) String apiKey,
             @RequestBody PaymentCallbackDTO callback) {
-
-        if (!validatePaymentServiceApiKey(apiKey)) {
-            Map<String, Object> errorResponse = new HashMap<>();
-            errorResponse.put("status", "error");
-            errorResponse.put("message", "Unauthorized access");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
-        }
-
         try {
             enrollmentService.processPaymentCallback(callback);
 
@@ -72,18 +62,5 @@ public class EnrollmentManagementController {
             errorResponse.put("message", "Terjadi kesalahan dalam memproses callback");
             return ResponseEntity.badRequest().body(errorResponse);
         }
-    }
-
-    private boolean validatePaymentServiceApiKey(String apiKey) {
-        return paymentServiceApiKey != null && paymentServiceApiKey.equals(apiKey);
-    }
-
-    // TODO : Implement JWT Token Auth
-    private Long extractStudentIdFromToken(String token) {
-        if (token != null && token.startsWith("Bearer ")) {
-            token = token.substring(7);
-        }
-        // Placeholder: returning a dummy Long ID instead of String
-        return 12345L;
     }
 }
